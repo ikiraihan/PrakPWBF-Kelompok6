dd91f3161963bcafe28194d5bc896162803d3f32
Your Name <you@example.com>
2021-12-08 09:00:47 +0700

    bikin home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,71 @@
-@extends('layouts.main')
+<!doctype html>
+<html lang="en" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>Home | RK Boutique</title>
 
-@section('container')
-<h1>Boutique Sembarang</h1><br>
-<img src="img/logo.png" alt="Beautique Sembarang" width="200">
-<h5><br>Jl. Sembarang No. 99 </h5>
-<h5>Kota Surabaya, Jawa Timur </h5>
-<p>Contact : +6289765432109 <br> Email &nbsp;&nbsp;&nbsp; : boutiquesembarang@gmail.com</p>
-@endsection
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/cover/">
+
+    
+
+    <!-- Bootstrap core CSS -->
+<link href="{{ asset('vendor/homeassets/css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="{{ asset('vendor/homeassets/css/bg.css') }}" rel="stylesheet">
+  </head>
+  <body class="d-flex h-100 text-center text-white bg-home">
+    
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="mb-auto">
+    <div>
+      <h3 class="float-md-start mb-0">RK Boutique</h3>
+      <nav class="nav nav-masthead justify-content-center float-md-end">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <li class="nav-item">
+                  <a class="nav-link" href="/login">
+                      <i class="fa fa-sign-in-alt"></i>
+                      Login 
+                  </a>
+        </li>
+      </nav>
+    </div>
+  </header>
+
+  <main class="px-3">
+    <h1>Welcome To RK Boutique.</h1>
+    <p class="lead">The Home of Unique,Stylish,New Fashion.</p>
+    <p class="lead">
+      <a href="/login" class="btn btn-lg btn-secondary fw-bold border-white bg-dark">Login</a>
+    </p>
+  </main>
+
+  <footer class="mt-auto text-white-50">
+  </footer>
+</div>
+
+
+    
+  </body>
+</html>
